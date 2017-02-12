982cc8429aa20fad6177d2226c921621dd952c19
Javier Bórquez <hi@javier.xyz>
2017-02-12 12:11:13 -0800

    Move folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    hey2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+* {
+	margin: 0;
+	padding: 0;
+	position: relative;
+	font-family: Helvetica Neue, Arial, sans-serif;
+	font-weight: 300;
+}
+</style>
