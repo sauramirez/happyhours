26acf2b76386261914cc5ee75cb1aafdaeccb575
Javier Bórquez <hi@javier.xyz>
2017-02-12 12:19:04 -0800

    Add cors support

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    hey2
+  <div class="main">
+    <div class="content">
+    	<h1>Happy Hour</h1>
+    </div>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
+
 export default {
-  name: 'app'
+  name: 'app',
+  mounted() {
+  	axios.get('http://startupbears.com/api/sentiment').then(res => {
+  		console.log(res)
+  	})
+  }
 }
 </script>
 
@@ -17,5 +26,19 @@ export default {
 	position: relative;
 	font-family: Helvetica Neue, Arial, sans-serif;
 	font-weight: 300;
+}
+
+h1 {
+	font-weight: 100;
+	font-size: 4rem;
+}
+
+.main {
+	max-width: 980px;
+	margin: 0 auto;
+}
+
+.content {
+	padding: 3rem;
 }
 </style>
